51c1582735f2aaaa870d5e7a8ec086604044c124
GODFREY MATAGARO <116557155+GodyRacks@users.noreply.github.com>
2023-06-13 14:06:15 +0300

    RaisedButton.kt

--- app/src/main/java/com/gody/jetpackcomposeuielements/uielements/buttons/RaisedButton.kt
+++ app/src/main/java/com/gody/jetpackcomposeuielements/uielements/buttons/RaisedButton.kt
@@ -1,4 +1,47 @@
-package com.gody.jetpackcomposeuielements.UIElements.buttons
+package com.gody.jetpackcomposeuielements.uielements.buttons
 
-class RaisedButton {
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ButtonDemo() {
+    // [START android_compose_layout_material_button]
+    Button(
+        onClick = { /* ... */ },
+        // Uses ButtonDefaults.ContentPadding by default
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+    ) {
+        // Inner content including an icon and a text label
+        Icon(
+            Icons.Filled.Favorite,
+            contentDescription = "Favorite",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Gody")
+    }
+    // [END android_compose_layout_material_button]
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RaisedButtonPrev() {
+    ButtonDemo()
 }
